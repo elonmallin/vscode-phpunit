12e4d957c1a68986f1c566eb0b7154feb4a95f2b
Eric Hansen <echansen@nabancard.com>
2017-09-05 21:16:02 -0400

    Support choosing specific test if desired

--- src/phpunittest.ts
+++ src/phpunittest.ts
@@ -17,10 +17,24 @@ export function runTestDirectory() {
     console.error("Couldn't determine directory. Make sure you have a file open in the directory you want to test.");
 }
 
+function execPhpUnit(execPath, args, outputChannel)
+{
+    let phpunitProcess = cp.spawn(execPath, args, { cwd: vscode.workspace.rootPath });
+    outputChannel.appendLine(execPath + ' ' + args.join(' '));
+
+    phpunitProcess.stderr.on("data", (buffer: Buffer) => {
+        outputChannel.append(buffer.toString());
+    });
+    phpunitProcess.stdout.on("data", (buffer: Buffer) => {
+        outputChannel.append(buffer.toString());
+    });
+}
+
 function execTest(directory: string) {
     let config = vscode.workspace.getConfiguration("phpunit");
     let execPath = config.get<string>("execPath", "phpunit");
     let configArgs = config.get<Array<string>>("args", []);
+    let forceClassRunWhenWithinTest = config.get<boolean>("forceClassTest", true);
 
     const editor = vscode.window.activeTextEditor;
     if (editor != undefined) {
@@ -29,6 +43,7 @@ function execTest(directory: string) {
             var wordOnCursor = editor.document.getText(range);
             let line = editor.document.lineAt(range.start.line);
             var isFunction = line.text.indexOf("function") != -1;
+            var isClass = line.text.indexOf("class") != -1;
         }
     }
 
@@ -39,31 +54,72 @@ function execTest(directory: string) {
     if (directory != null && directory != "")
     {
         args.push(directory);
+
+        execPhpUnit(execPath, args, outputChannel);
     }
     else
     {
-        if (isFunction && wordOnCursor != null)
-        {
+        if (isFunction && wordOnCursor != null) {
             args.push("--filter");
             args.push(wordOnCursor);
-        }
-        if (editor != undefined && editor.document.fileName != null)
-        {
+
             let relPath = editor.document.uri.fsPath;
             args.push(relPath);
+
+            execPhpUnit(execPath, args, outputChannel);
         }
+        else if (editor != undefined && editor.document.fileName != null)
+        {
+            if (isClass || forceClassRunWhenWithinTest)
+            {
+                let relPath = editor.document.uri.fsPath;
+                args.push(relPath);
+
+                execPhpUnit(execPath, args, outputChannel);
+            }
+            else
+            {
+                let windowText = editor.document.getText();
+                let testFunctions = [];
+                let regex = /\s*(abstract|final|private|protected|public|static)\s+function\s+(test.*)\s*\(/gi;
+                let result = null;
+                let currentTest = null;
+                let startPosition = editor.selection.active.line;
+                
+                while (!testFunctions.length && startPosition > -1) {
+                    let line = editor.document.lineAt(startPosition);
+
+                    if ((result = regex.exec(line.text))) {
+                        testFunctions.push(result[2].toString().trim());
+                    }
+
+                    startPosition--;
+                }
+
+                while ((result = regex.exec(windowText))) {
+                    let testToAdd = result[2].toString().trim();
+
+                    if (!testFunctions.length || testFunctions[0] != testToAdd) {
+                        testFunctions.push(testToAdd);
+                    }    
+                }
+
+                if (testFunctions.length > 0) {
+                    vscode.window.showQuickPick(testFunctions, {}).then(function (selectedTest) {
+                        if (undefined !== selectedTest) {
+                            args.push("--filter");
+                            args.push(selectedTest);
+
+                            let relPath = editor.document.uri.fsPath;
+                            args.push(relPath);
+
+                            execPhpUnit(execPath, args, outputChannel);
+                        }    
+                    });
+                } else {
+                    execPhpUnit(execPath, args, outputChannel);
+                }    
+            }    
+        } 
     }
-    
-    let phpunitProcess = cp.spawn(execPath, args, { cwd: vscode.workspace.rootPath });
-    outputChannel.appendLine(execPath + ' ' + args.join(' '));
-    
-    phpunitProcess.stderr.on("data", (buffer: Buffer) => {
-        outputChannel.append(buffer.toString());
-    });
-    phpunitProcess.stdout.on("data", (buffer: Buffer) => {
-        outputChannel.append(buffer.toString());
-    });
-    /*phpunitProcess.on("close", (code: string) => {
-        outputChannel.hide();
-    });*/
 }
