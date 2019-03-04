ccc561a02904ac22367f86539a2cec9115b71ee7
Elon Mallin <elon.mallin@gmail.com>
2019-03-04 21:03:50 +0100

    add PhpUnitResolver

---
+++ src/Drivers/PhpUnitResolver.ts
@@ -0,0 +1,58 @@
+import * as vscode from 'vscode';
+import PhpUnitDriverInterface from "./PhpUnitDriverInterface";
+import Path from "./PathDriver";
+import Composer from "./ComposerDriver";
+import Phar from "./PharDriver";
+import GlobalPhpUnit from "./GlobalPhpUnitDriver";
+
+const phpUnitPath = async (): Promise<string> => {
+    if (this._phpUnitPath)
+    {
+        return this._phpUnitPath;
+    }
+
+    const config = vscode.workspace.getConfiguration('phpunit');
+    const order = config.get<string[]>('driverPriority');
+    const drivers = await this.getDrivers(order);
+
+    for (let driver of drivers) {
+        this._phpUnitPath = await driver.phpUnitPath();
+        if (this._phpUnitPath) {
+            return this._phpUnitPath;
+        }
+    }
+
+    return null;
+}
+
+const getDrivers = (order?: string[]): PhpUnitDriverInterface[] => {
+    const drivers: PhpUnitDriverInterface[] = [
+        new Path(),
+        new Composer(),
+        new Phar(),
+        new GlobalPhpUnit(),
+    ];
+
+    function arrayUnique(array) {
+        var a = array.concat();
+        for(var i=0; i<a.length; ++i) {
+            for(var j=i+1; j<a.length; ++j) {
+                if(a[i] === a[j])
+                    a.splice(j--, 1);
+            }
+        }
+    
+        return a;
+    }
+    order = arrayUnique((order || []).concat(drivers.map(d => d.name)));
+
+    const sortedDrivers = drivers.sort((a, b) => {
+        return order.indexOf(a.name) - order.indexOf(b.name);
+    });
+
+    return sortedDrivers;
+}
+
+export {
+    phpUnitPath as resolvePhpUnitPath
+};
