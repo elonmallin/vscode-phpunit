c93e85fdcf6cef51242e5ade73acabff2caeeabe
Elon Mallin <elon.mallin@gmail.com>
2019-03-04 21:53:26 +0100

    add `phpunit.command` config for `CommandDriver`

--- src/Drivers/CommandDriver.ts
+++ src/Drivers/CommandDriver.ts
@@ -5,14 +5,12 @@ import { resolvePhpUnitPath } from './PhpUnitResolver';
 
 export default class Command implements PhpUnitDriverInterface {
     name: string = 'Command';
-    private _phpPath: string;
+    private _command: string;
     private _phpUnitPath: string;
 
     public async run(args: string[]): Promise<RunConfig> {
-        const phpPath = await this.phpPath();
         args = [await this.phpUnitPath()].concat(args);
-
-        const command = `${phpPath} ${args.join(' ')}`;
+        const command = `${await this.command()} ${args.join(' ')}`;
 
         return {
             command: command
@@ -20,16 +18,17 @@ export default class Command implements PhpUnitDriverInterface {
     }
 
     public async isInstalled(): Promise<boolean> {
-        return (await this.phpPath()) != null && (await this.phpUnitPath()) != null;
+        return (await this.command()) != null && (await this.phpUnitPath()) != null;
     }
 
-    async phpPath(): Promise<string> {
-        return this._phpPath
-            || (this._phpPath = vscode.workspace.getConfiguration('phpunit').get<string>('php'));
+    async command(): Promise<string> {
+        return this._command
+            || (this._command = vscode.workspace.getConfiguration('phpunit').get<string>('command'));
     }
 
     async phpUnitPath(): Promise<string> {
         return this._phpUnitPath
+            || (this._phpUnitPath = vscode.workspace.getConfiguration('phpunit').get<string>('phpunit'))
             || (this._phpUnitPath = await resolvePhpUnitPath());
     }
 }
