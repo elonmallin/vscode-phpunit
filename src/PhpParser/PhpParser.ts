7cdc9fa4a8a6928f57fae5b1096e0dc04f0b2da0
Elon Mallin <elon@boxnet.se>
2019-07-15 08:41:55 +0200

    refactor: fix code style

--- src/PhpParser/PhpParser.ts
+++ src/PhpParser/PhpParser.ts
@@ -43,7 +43,7 @@ export default async function parse(filePath: string) {
   });
 
   const parsed = parsePhpToObject(phpClassAsString);
-  parsed.name = path.basename(filePath, '.php');
+  parsed.name = path.basename(filePath, ".php");
 
   return parsed;
 }
