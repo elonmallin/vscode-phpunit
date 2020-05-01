b6ec1fbf2ed72da4304e58bdca3dff5fb39b06f5
Elon Mallin <elon.mallin@gmail.com>
2020-05-01 23:39:18 +0200

    feat(ssh): add ssh functionality and mark it as deprecated right away since it's really better to use the Remote Development Extension.

--- src/Drivers/SshDriver.ts
+++ src/Drivers/SshDriver.ts
@@ -1,16 +1,49 @@
+import * as vscode from "vscode";
 import { IRunConfig } from "../RunConfig";
 import IPhpUnitDriver from "./IPhpUnitDriver";
+import { resolvePhpUnitPath } from "./PhpUnitResolver";
 
 export default class Ssh implements IPhpUnitDriver {
   public name: string = "Ssh";
-  public run(args: string[]): Promise<IRunConfig> {
-    throw new Error("Method not implemented.");
-  }
-  public async isInstalled(): Promise<boolean> {
-    return false;
+  private phpPathCache: string;
+  private phpUnitPathCache: string;
+  private ssh: string;
+
+  public async run(args: string[]): Promise<IRunConfig> {
+    return {
+      command: `${this.ssh} "${this.phpPathCache} ${this.phpUnitPathCache} ${args.join(" ")}"`
+    };
   }
 
-  public phpUnitPath(): Promise<string> {
-    throw new Error("Method not implemented.");
+  public async isInstalled(): Promise<boolean> {
+    const config = vscode.workspace.getConfiguration("phpunit");
+    this.ssh = config.get<string>("ssh");
+
+    return !!(this.ssh && (await this.phpPath()) && (await this.phpUnitPath()));
+  }
+
+  public async phpPath(): Promise<string> {
+    if (this.phpPathCache) {
+      return this.phpPathCache;
+    }
+
+    const config = vscode.workspace.getConfiguration("phpunit");
+    this.phpPathCache = config.get<string>("php", "php"); // Use default `php` for this driver since we probably can assume `php` is on path.
+
+    return this.phpPathCache;
+  }
+
+  public async phpUnitPath(): Promise<string> {
+    if (this.phpUnitPathCache) {
+      return this.phpUnitPathCache;
+    }
+
+    const config = vscode.workspace.getConfiguration("phpunit");
+    this.phpUnitPathCache = config.get<string>("phpunit");
+
+    return (
+      this.phpUnitPathCache ||
+      (this.phpUnitPathCache = await resolvePhpUnitPath())
+    );
   }
 }
