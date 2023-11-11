3b8cb4815e76a5783d16c67ff2b563ddfda8a026
Elon Mallin <elon.mallin@gmail.com>
2023-11-11 11:35:13 +0100

    test: add test sample

---
+++ src/test/suite/index.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import { glob } from 'glob';
+
+export async function run(): Promise<void> {
+	// Create the mocha test
+	const mocha = new Mocha({
+		ui: 'tdd'
+	});
+ 
+	const testsRoot = path.resolve(__dirname, '..');
+
+  const files = await glob('**/**.test.js', { cwd: testsRoot });
+
+  // Add files to the test suite
+  files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+  // Run the mocha test
+  return new Promise((resolve, reject) => {
+    mocha.run(failures => {
+        if (failures > 0) {
+            reject(new Error(`${failures} tests failed.`));
+        } else {
+            resolve();
+        }
+    });
+  });
+}
