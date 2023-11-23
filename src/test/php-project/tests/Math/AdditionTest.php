87552c3346e60b1a61f860243ffedf0bf2282031
Elon Mallin <elon.mallin@gmail.com>
2023-11-23 22:12:34 +0100

    fix: make phpunit math test easier

--- src/test/php-project/tests/Math/AdditionTest.php
+++ src/test/php-project/tests/Math/AdditionTest.php
@@ -6,7 +6,7 @@ use Math\Addition;
 class AdditionTest extends TestCase {
     public function testAdd() {
         $sut = new Addition();
-        $result = $sut->add(5, 3);
-        $this->assertEquals(8, $result);
+        $result = $sut->add(5, 5);
+        $this->assertEquals(10, $result);
     }
 }
